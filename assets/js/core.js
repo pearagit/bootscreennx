41546f943b0f02b1d20b2394f62d461df0b27c45
Harry Peach <harrypeach1999@gmail.com>
2019-05-07 16:20:20 +0100

    Implemented custom text drawing and jsdocs

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,11 +1,61 @@
+// The preview canvas
 const drawCanvas = document.querySelector("#bootPreview");
+// The context of the preview canvas
 const drawCanvasCtx = drawCanvas.getContext("2d");
+// Width of the Nintendo Switch screen, and by extension, the canvas
+const CANVAS_WIDTH = 1280;
+// Height of the Nintendo Switch screen, and by extension, the canvas
+const CANVAS_HEIGHT = 720;
+
+/**
+ * Draws a string of text, with accurate character spacing
+ * @param {string} text The text to write to the screen
+ * @param {number} x The x co-ordinate of the text
+ * @param {number} y The y co-ordinate of the text
+ * @param {string} color The color in which to write the text
+ */
+function drawText(text, x, y, color = 'gray'){
+    drawCanvasCtx.font = "32px PerfectDOSVGA437Win";
+    drawCanvasCtx.fillStyle = color;
+    drawCanvasCtx.textBaseline = "top"; 
+
+    for (var i = 0; i < text.length; i++) {
+        drawCanvasCtx.fillText(text.charAt(i), x + (i * 16), y - 4);
+    }
+}
+
+/**
+ * Resets and redraws all elements on the canvas
+ */
+function redrawCanvas(){
+    // The type of firmware the user is running
+    var cfwType = $('select[name=type] option:selected', "#settings");
+    // The firmware version selected by the user
+    var firmwareVersion = $('select[name=firmware] option:selected', "#settings");
+    // The size of the internal eMMC
+    var emmcSize = $('select[name=emmc] option:selected', "#settings");
+    // The size of the external SD
+    var sdSize = $('select[name=sd] option:selected', "#settings");
+    // Copyright information, at the bottom of the screen
+    var copyrightLine = '';
+
+    // Reset the canvas and draw the black background
+    drawCanvasCtx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    drawCanvasCtx.fillStyle = "black";
+    drawCanvasCtx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    // Draw any text the user requests
+    drawText(cfwType.text(), 64, 16);
+    drawText("Copyright (C) 2019, Team ReSwitched", 64, 48);
+}
 
 // Every time an input is changed, modify the preview
 $("#settings input, #settings select").on('change', function() {
-    console.log("Change");
+    console.log("Settings change");
+    redrawCanvas();
 });
 
-drawCanvasCtx.moveTo(0, 0);
-drawCanvasCtx.lineTo(200, 100);
-drawCanvasCtx.stroke();
+// Draw the canvas once the window has loaded
+window.onload = function() {
+    redrawCanvas();
+};
